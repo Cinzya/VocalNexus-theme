183ed11f1095a196e2a48ede4f1952a88e08a6ed
Cinzya <cynti-ebert@web.de>
2020-05-23 21:40:08 +0200

    added about us page

---
+++ about-us.php
@@ -0,0 +1,61 @@
+<?php
+/* Template Name: About us */
+?>
+
+<?php get_header();?>
+
+<section class="about-us">
+    <div class="text">
+
+    </div>
+
+    <div class="members">
+        <h2>Das Team</h2>
+        <div class="member">
+            <img src="" alt="">
+            <h3>Freaky Lulu</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>Railgun</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>TheVocalOne</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>ParkerSelfridge</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>LenGusseKun</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>Maki</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>RebornPhoenix</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>frozenice</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>Richman1986</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>Riku</h3>
+        </div>
+        <div class="member">
+            <img src="" alt="">
+            <h3>Akeno</h3>
+        </div>
+    </div>
+</section>
+
+<?php get_footer();?>
