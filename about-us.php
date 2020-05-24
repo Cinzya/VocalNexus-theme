4eed6db9b0f30909f93bfb7e5a3c17807e47491c
Cinzya <cynti-ebert@web.de>
2020-05-25 00:02:00 +0200

    added members pics

--- about-us.php
+++ about-us.php
@@ -12,48 +12,81 @@
     <div class="members">
         <h2>Das Team</h2>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/freakylulubig.png") ?>" alt="">
             <h3>Freaky Lulu</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/railgun-big.png") ?>" alt="">
             <h3>Railgun</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/thevocalone-big.png") ?>" alt="">
             <h3>TheVocalOne</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/parkerselfridge-big.png") ?>" alt="">
             <h3>ParkerSelfridge</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/lengussekun-big.png") ?>" alt="">
             <h3>LenGusseKun</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/maki-big.png") ?>" alt="">
             <h3>Maki</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/rebornphoenix-big.png") ?>" alt="">
             <h3>RebornPhoenix</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/frozenice-big.png") ?>" alt="">
             <h3>frozenice</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/richman-big.png") ?>" alt="">
             <h3>Richman1986</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/riku-big.png") ?>" alt="">
             <h3>Riku</h3>
+            <div class="socials">
+                
+            </div>
         </div>
         <div class="member">
-            <img src="" alt="">
+            <img src="<?php echo get_theme_file_uri("/img/members/akeno-big.png") ?>" alt="">
             <h3>Akeno</h3>
+            <div class="socials">
+                
+            </div>
         </div>
     </div>
 </section>
